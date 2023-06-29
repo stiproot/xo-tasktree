e8f807a4af748f4b5fe325a668539aa52d272be4
stiproot <stipcich.simon@gmail.com>
2023-06-29 18:26:51 +0200

    feat:fixed broken if-then-if-then test.

--- test/unit/Xo.TaskTree.Unit.Tests/StateManagerTests.cs
+++ test/unit/Xo.TaskTree.Unit.Tests/StateManagerTests.cs
@@ -57,17 +57,18 @@ public class StateManagerTests
 			.RootIf<IY_OutConstBool_SyncService>()
 			.Then<IY_InStr_OutConstInt_AsyncService>(
 				configure => configure.MatchArg("<<arg>>"),
-				then => then.If<IY_InInt_OutBool_SyncService>(configure: c => c.RequireResult()).Then<IY_InBool_OutConstStr_AsyncService>().Else<IY_AsyncService>()
+				then => then.If<IY_InInt_OutBool_SyncService>(configure: c => c.RequireResult()).Then<IY_InBool_OutConstStr_AsyncService>(c => c.RequireResult()).Else<IY_AsyncService>()
 			)
 			.Else<IY_InStr_AsyncService>(c => c.MatchArg<IY_InStr_OutConstStr_AsyncService>(c => c.MatchArg("<<arg>>")));
 
 			var n = mn.Build();
 
 			var msgs = await n.Run(cancellationToken);
-			// var msg = msgs.Second(); 
-			// var d = (msg as Msg<int>)!.GetData();
+			var msg = msgs.First(); 
+			var d = (msg as Msg<string>)!.GetData();
 
-			Assert.Equal(1, 1);
+			Assert.NotNull(d);
+			Assert.IsType<Guid>(Guid.Parse(d));
 	}
 
 	[Fact]
